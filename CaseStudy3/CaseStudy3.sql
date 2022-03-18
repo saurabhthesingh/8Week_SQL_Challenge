3ad43b1a4ee9f93914e64878e4ef6aa55fbe05e2
Saurabh Singh <59935481+saurabhthesingh@users.noreply.github.com>
2022-03-18 14:01:56 +0530

    Update CaseStudy3.sql

--- CaseStudy3/CaseStudy3.sql
+++ CaseStudy3/CaseStudy3.sql
@@ -83,7 +83,80 @@ ORDER BY next_plan ;
   
   
 --7. What is the customer count and percentage breakdown of all 5 plan_name values at 2020-12-31?
+WITH next_plan AS(
+SELECT 
+  customer_id, 
+  plan_id, 
+  start_date,
+  LEAD(start_date, 1) OVER(PARTITION BY customer_id ORDER BY start_date) as next_date
+FROM foodie_fi.subscriptions
+WHERE start_date <= '2020-12-31')
+,
+-- To find breakdown of customers with existing plans on or after 31 Dec 2020
+customer_breakdown AS (
+  SELECT 
+	plan_id, 
+	COUNT(DISTINCT customer_id) AS customers
+  FROM next_plan
+  WHERE (next_date IS NOT NULL AND (start_date < '2020-12-31' AND next_date > '2020-12-31'))
+      OR (next_date IS NULL AND start_date < '2020-12-31')
+  GROUP BY plan_id)
+
+SELECT 
+ plan_id, 
+ customers, 
+ ROUND(100 * customers::NUMERIC / (SELECT COUNT(DISTINCT customer_id) FROM foodie_fi.subscriptions),1) AS percentage
+FROM customer_breakdown
+GROUP BY plan_id, customers
+ORDER BY plan_id
+
+
 --8. How many customers have upgraded to an annual plan in 2020?
+SELECT 
+ COUNT(*) as customers,
+FROM foodie_fi.subscriptions
+WHERE plan_id = 3
+AND start_date <= '2020-12-31'
+
 --9. How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
+-- Filter results to customers at trial plan = 0
+WITH trial_plan AS 
+(SELECT 
+  customer_id, 
+  start_date AS trial_date
+FROM foodie_fi.subscriptions
+WHERE plan_id = 0
+),
+-- Filter results to customers at pro annual plan = 3
+annual_plan AS
+(SELECT 
+  customer_id, 
+  start_date AS annual_date
+FROM foodie_fi.subscriptions
+WHERE plan_id = 3
+)
+
+SELECT 
+  ROUND(AVG(annual_date - trial_date),0) AS avg_days_to_upgrade
+FROM trial_plan tp
+JOIN annual_plan ap
+  ON tp.customer_id = ap.customer_id;
+
 --10. Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+
+
 --11. How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
+WITH next_plan_cte AS (
+SELECT 
+  customer_id, 
+  plan_id, 
+  start_date,
+  LEAD(plan_id, 1) OVER(PARTITION BY customer_id ORDER BY plan_id) as next_plan
+FROM foodie_fi.subscriptions)
+
+SELECT 
+  COUNT(*) AS downgraded
+FROM next_plan_cte
+WHERE start_date <= '2020-12-31'
+  AND plan_id = 2 
+  AND next_plan = 1;
